42ed0dfdd19ab65f24213b7274008c22e9a838e3
Roberto Albanesi <beto0607@gmail.comn>
2019-05-31 14:29:11 +0200

    updated header, added styles

--- src/components/header.js
+++ src/components/header.js
@@ -7,18 +7,13 @@ class Header extends Component{
     constructor(props){
         super(props);
         this.sections = props.sections || [];
-        this._onClick = this._onClick.bind(this);
-
-    }
-    _onClick({target}){
-        console.log(target.href);
     }
     render(){
         return (
             <header>
                 {
                     this.sections.map((element, index)=>(
-                        <a href={"#"+element.id} onClick={this._onClick} key={"header#"+index}>{element.name}</a>
+                        <a href={"#"+element.id} key={"header#"+index}>{element.name}</a>
                     ))
                 }
             </header>
