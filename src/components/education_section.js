be8002d9144bc0ac67cd02dcd6f8c5607f24aff6
Roberto Albanesi <beto0607@gmail.comn>
2019-05-31 15:57:09 +0200

    Close #4

--- src/components/education_section.js
+++ src/components/education_section.js
@@ -5,7 +5,7 @@ import SectionContainer from "./section"
 const EducationSection = (props)=>{
     return (
         <SectionContainer name="education" id={props.id} {...props}>
-            {props.children}
+            
         </SectionContainer>
     )
 }
