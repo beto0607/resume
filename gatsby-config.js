8e57d1f484aa0138f440432fe5036c41bbc0f6f9
Roberto Albanesi <beto0607@gmail.comn>
2019-06-08 21:45:10 +0200

    Added profile to codecademy. Added favicon. Removed unused images. Changed page title

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,10 +29,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1b6580`,
+        theme_color: `#1b6580`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/page_icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-transformer-json`,
