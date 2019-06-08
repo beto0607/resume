83ea43e36b656a4fbae034dbf4958178ee2e37a0
Roberto Albanesi <beto0607@gmail.comn>
2019-06-08 08:29:32 +0200

    Removed Knowledge section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,45 +4,39 @@ import sectionStyles from "../scss/section.module.scss"
 import Header from "../components/header"
 import PersonalSection from "../components/personal_section"
 import ProjectSection from "../components/project_section"
-import KnowledgeSection from "../components/knowledge_section"
 import CoverLetterSection from "../components/cover_letter_section"
 import EducationSection from "../components/education_section"
 import OthersSection from "../components/others_section"
 import typography from '../utils/typography'
 
 const IndexPage = () => {
-	typography.injectStyles()
-	const personal_id = sectionStyles['personal_section'];
-	const projects_id = sectionStyles['projects_section'];
-	const cover_letter_id = sectionStyles['cover_letter_section'];
-	const knowledge_id = sectionStyles['knowledge_section'];
-	const education_id = sectionStyles['education_section'];
-	const others_id = sectionStyles['others_section'];
-	console.log(others_id);
-	return (
-  <React.Fragment>
-		<PersonalSection 
-			id={personal_id}/>
-		<ProjectSection 
-			id={projects_id}/>
-{/*		<KnowledgeSection 
-id={knowledge_id}/>
-		<OthersSection 
-        id={others_id}/>
-        {id: knowledge_id, name:"Knoweldge"},
-        {id: others_id, name:"More"}
-*/}
-		<CoverLetterSection 
-			id={cover_letter_id}/>
-		<EducationSection
-			id={education_id}/>
-		<Header sections={[
-			{id: personal_id, name:"Information"},
-			{id: projects_id, name:"Projects"},
-			{id: cover_letter_id, name:"Cover letter"},
-			{id: education_id, name:"Education"},
-		]}/>
-  </React.Fragment>)
+    typography.injectStyles()
+    const personal_id = sectionStyles['personal_section'];
+    const projects_id = sectionStyles['projects_section'];
+    const cover_letter_id = sectionStyles['cover_letter_section'];
+    const education_id = sectionStyles['education_section'];
+    const others_id = sectionStyles['others_section'];
+    console.log(others_id);
+    return (
+        <React.Fragment>
+            <PersonalSection
+                id={personal_id} />
+            <ProjectSection
+                id={projects_id} />
+            <CoverLetterSection
+                id={cover_letter_id} />
+            <EducationSection
+                id={education_id} />
+            <OthersSection
+                id={others_id} />
+            <Header sections={[
+                { id: personal_id, name: "Information" },
+                { id: projects_id, name: "Projects" },
+                { id: cover_letter_id, name: "Cover letter" },
+                { id: education_id, name: "Education" },
+                { id: others_id, name: "More" }
+            ]} />
+        </React.Fragment>)
 }
 
 export default IndexPage;
