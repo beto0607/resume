da2737bd091abccd54019c08f054b1801395b649
Roberto Albanesi <beto0607@gmail.comn>
2019-06-30 23:14:15 +0200

    Created Knowledge section. Added to index.js and section.module.scss

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import ProjectSection from "../components/project_section"
 import CoverLetterSection from "../components/cover_letter_section"
 import EducationSection from "../components/education_section"
 import typography from '../utils/typography'
+import Knowledge from "../components/Knowledge/Knowledge";
 
 const IndexPage = () => {
     typography.injectStyles()
@@ -15,14 +16,15 @@ const IndexPage = () => {
     const projects_id = sectionStyles['projects_section'];
     const cover_letter_id = sectionStyles['cover_letter_section'];
     const education_id = sectionStyles['education_section'];
+    const knowledge_id = sectionStyles['knowledge_section'];
     const others_id = sectionStyles['others_section'];
     console.log(others_id);
     return (
         <React.Fragment>
             <Helmet>
                 <title>Resume - Roberto Albanesi</title>
-                <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"/>
-                <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet"/>
+                <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet" />
             </Helmet>
             <PersonalSection
                 id={personal_id} />
@@ -32,11 +34,14 @@ const IndexPage = () => {
                 id={projects_id} />
             <EducationSection
                 id={education_id} />
+            <Knowledge
+                id={knowledge_id} />
             <Header sections={[
                 { id: personal_id, name: "Information" },
                 { id: cover_letter_id, name: "Cover letter" },
                 { id: projects_id, name: "Projects" },
-                { id: education_id, name: "Education" }
+                { id: education_id, name: "Education" },
+                { id: knowledge_id, name: "Knowledge" }
             ]} />
         </React.Fragment>)
 }
